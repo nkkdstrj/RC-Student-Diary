e4504b44b90829a6d623e70dd1c33684b5178713
nkkdstrj <nkkdstrj@gmail.com>
2023-09-19 21:31:48 +0800

    2ND MAJOR COMMIT

--- app/src/main/java/com/rcdiarycollegedept/rcstudentdiary/HomeFragment.java
+++ app/src/main/java/com/rcdiarycollegedept/rcstudentdiary/HomeFragment.java
@@ -2,23 +2,49 @@ package com.rcdiarycollegedept.rcstudentdiary;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rcdiarycollegedept.rcstudentdiary.databinding.ActivityHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-    }
+
+    private TextView NameTextView;
+    private DatabaseReference databaseReference;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        NameTextView = rootView.findViewById(R.id.NameTextView);
+        // Get the reference to the Firebase Realtime Database node
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child("FullName");
+        // Call the method to retrieve data from the database
+        fetchDataFromFirebase();
+        return rootView;
+    }
+
+    private void fetchDataFromFirebase() {
+        // Read data from the Firebase database
+        databaseReference.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful() && task.getResult() != null && task.getResult().getValue() != null) {
+                String data = task.getResult().getValue().toString();
+                // Update the TextView with the retrieved data
+                NameTextView.setText(data);
+            }else {
+                // Handle the error
+                NameTextView.setText("Error");
+            }
+        });
     }
 }
